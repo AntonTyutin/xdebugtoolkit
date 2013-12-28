e87332e4bd1ab25b418fd9855364198c10f1081b
Anton Tyutin <anton@tyutin.ru>
2013-12-28 23:55:52 +0400

    Add timing percentage

--- dot.py
+++ dot.py
@@ -9,15 +9,17 @@ class DotBuilder:
         graph.append('digraph G { \n')
         #graph.append('ordering=out; \n') # dot fails if rankdir=LR and ordering=out
         graph.append('rankdir=TB; \n')
-        graph.append('edge [labelfontsize=12]; \n')
-        graph.append('node [shape=box, style=filled]; \n')
+        graph.append('edge [fontsize="11"]; \n')
+        graph.append('node [fontsize="12" shape="box" style="filled"]; \n')
         
         stack = [tree.root_node]
         stack_pos = [-1, 0]
         
+        total_time = tree.get_total_time()
+        
         self_id = '/'.join(map(str, stack_pos[0:-1]));
         graph.append('"%s" [label="root"];\n' % (self_id, ))
-        
+
         while len(stack):
             try:
                 stack.append(stack[-1].subcalls[stack_pos[-1]])
@@ -35,13 +37,13 @@ class DotBuilder:
             if len(fn) > 30:
                 fn = fn[0:12] + '...' + fn[-15:]
             
-            linewidth = 6.0 * stack[-1].sum_inclusive_time/tree.get_total_time() + 1
+            linewidth = 19.0 * stack[-1].sum_inclusive_time/total_time + 1
             if stack[-1].call_count == 1:
-                node_label = '%s\\n%dms' % (fn, stack[-1].sum_self_time/1000)
-                edge_label = '%dms' % (stack[-1].sum_inclusive_time/1000)
+                node_label = '%s\\n%dms / %.1f%%' % (fn, stack[-1].sum_self_time/1000, 100. * stack[-1].sum_self_time/total_time)
+                edge_label = '%dms / %.1f%%' % (stack[-1].sum_inclusive_time/1000, 100. * stack[-1].sum_inclusive_time/total_time)
             else:
-                node_label = '%s\\n%dx\[%dms..%dms] = %dms' % (fn, stack[-1].call_count, stack[-1].min_self_time/1000, stack[-1].max_self_time/1000, stack[-1].sum_self_time/1000)
-                edge_label = '%dx\[%dms..%dms] = %dms' % (stack[-1].call_count, stack[-1].min_inclusive_time/1000, stack[-1].max_inclusive_time/1000, stack[-1].sum_inclusive_time/1000)
+                node_label = '%s\\n%dx\[%dms..%dms] = %dms / %.1f%%' % (fn, stack[-1].call_count, stack[-1].min_self_time/1000, stack[-1].max_self_time/1000, stack[-1].sum_self_time/1000, 100. * stack[-1].sum_self_time/total_time)
+                edge_label = '%dx\[%dms..%dms] = %dms / %.1f%%' % (stack[-1].call_count, stack[-1].min_inclusive_time/1000, stack[-1].max_inclusive_time/1000, stack[-1].sum_inclusive_time/1000, 100. * stack[-1].sum_inclusive_time/total_time)
 
             graph.append('"%s" [label="%s" color="%s"]; \n' % (self_id, node_label, color))
             graph.append('"%s" -> "%s" [label="%s" style="setlinewidth(%s)" color="#AAAAFF"]; \n' % (parent_id, self_id, edge_label, linewidth))
